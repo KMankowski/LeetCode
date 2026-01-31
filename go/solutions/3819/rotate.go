17f5a5018ed656ba16d5217ee77f75ecc535cfd5
Kevin Mankowski <mankowskikevin@gmail.com>
2026-01-30 23:49:45 -0500

    feature: 3819 rotate non negative elements (#27)
    * feature: 3819 rotate non negative elements
    
    * fix: add main() func to main package

---
+++ go/solutions/3819/rotate.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(rotateElements([]int{1, -2, 3, -4}, 3))
+}
+
+func rotateElements(nums []int, k int) []int {
+	newNums := make([]int, len(nums))
+
+	nonNegativeNums := make([]int, 0)
+	originalIndices := make([]int, 0)
+	for i, num := range nums {
+		if num < 0 {
+			newNums[i] = num
+		} else {
+			nonNegativeNums = append(nonNegativeNums, num)
+			originalIndices = append(originalIndices, i)
+		}
+	}
+
+	if len(nonNegativeNums) == 0 {
+		return newNums
+	}
+
+	k = k % len(nonNegativeNums)
+	for i, num := range nonNegativeNums {
+		if i-k >= 0 {
+			newNums[originalIndices[i-k]] = num
+		} else {
+			newNums[originalIndices[i-k+len(nonNegativeNums)]] = num
+		}
+	}
+
+	return newNums
+}
